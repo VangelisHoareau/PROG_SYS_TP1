b56eeb0901418c65135f7247c24eac005fe564ed
Vangelis <vangelis_hoareau@yahoo.fr>
2021-10-06 09:02:48 +0200

    4eme commit - redaction

--- server_2.c
+++ server_2.c
@@ -7,7 +7,7 @@ int running =1;
 
 void stop_handler(int sig){
     printf("\nLe numero du signal est : %d\n", sig);
-    running=0;
+    //running=0;
 }
 
 int main(){
@@ -16,6 +16,7 @@ int main(){
     act.sa_handler=stop_handler;
     sigaction(SIGINT, &act, NULL);
     sigaction(SIGTERM, &act, NULL);
+    //sigaction(SIGKILL, &act, NULL);
     while(running){
         printf("pid fils : %d\n", getpid());
         printf("pid père : %d\n", getppid());
@@ -26,4 +27,19 @@ int main(){
     return EXIT_SUCCESS;
 }
 
-// Question 1.2
+/*
+Question 1.2
+
+Le message s'affiche lorsqu'on met l'option -s INT mais sans kill envoie le signal SIGTERM par défaut qui n'est donc pas traité par le sigaction.
+Pour afficher le message, il suffit d'ajouter une reception sigaction(SIGTERM, &act, NULL); dans le main. Cependant sur Mac cela ne fonctionne pas.
+
+Le signal Kill ne peut être traité, il est donc impossible d'afficher le message.
+Lorssqu'on donne le processus père :
+    - avec -s KILL : la bash est interrompu
+    - sans : il ne se passe rien 
+
+En enlevant le changement de la variable running : 
+    -le programme ne s'arrête pas avec CTRL-C mais au bout du 2ème le handler est appelé
+    - même chose pour kill
+    - Kill -9 arrête le processus sans appeler le handler
+*/
